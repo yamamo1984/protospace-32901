bae5a47f3445943db7118b8dc35eefc9ab2207c6
yamamo1984 <yytworldproducts@gmail.com>
2021-02-03 11:07:47 +0900

    ユーザー登録のバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
 
   has_many :prototypes
   has_many :comments
